7073a471d51d41518942d6479b37a756e98ec77b
Ankit Sharma <ankit.sharma@zones.com>
2021-03-07 14:27:19 +0530

    pushing latest changes

---
+++ src/app/shared/edit-popup-modal/edit-popup-modal.component.ts
@@ -0,0 +1,47 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { AngularFireStorage, AngularFireUploadTask  } from '@angular/fire/storage';   //   import <<<<
+
+
+@Component({
+  selector: 'app-edit-popup-modal',
+  templateUrl: './edit-popup-modal.component.html',
+  styleUrls: ['./edit-popup-modal.component.scss']
+})
+export class EditPopupModalComponent implements OnInit {
+
+  task: AngularFireUploadTask;  
+  downloadableURL = ''; 
+  file:any; 
+
+  constructor(
+    public dialogRef: MatDialogRef<EditPopupModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data:any,
+    private fireStorage: AngularFireStorage ) {}
+
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+
+
+  ngOnInit() {
+  }
+
+  async onSubmit(entityForm:NgForm){
+    console.log(entityForm.value);
+
+    const filePath = entityForm.value.imgName;  // path at which image will be stored in the firebase storage
+    this.task =  this.fireStorage.upload(filePath, this.file);    // upload task
+
+    // this.progress = this.snapTask.percentageChanges();
+
+    (await this.task).ref.getDownloadURL().then(url => {this.downloadableURL = url; });  // <<< url is found here
+
+  }
+
+  onFileChanged(event) {
+    this.file = event.target.files[0]; 
+  }
+
+}
