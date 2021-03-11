85c22fc08e0365865668851a6a8b70073eea4c1a
Ankit Sharma <ankit.sharma@zones.com>
2021-03-11 11:42:31 +0530

    endpoint in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
+const domainURl ='https://mount-xavier-services.herokuapp.com/'
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyB8E43HRMp46hdQNhDwP4lFEso2CeRjAkU",
+    authDomain: "mount-xavier-ui-16fd8.firebaseapp.com",
+    projectId: "mount-xavier-ui-16fd8",
+    storageBucket: "mount-xavier-ui-16fd8.appspot.com",
+    messagingSenderId: "378436192370",
+    appId: "1:378436192370:web:a5af5d3db9341981989aeb",
+    measurementId: "G-F00C8F2WPZ"
+  },
+  getCarousel:'https://mount-xavier-services.herokuapp.com/getAllCarousel',
+  addCarousel: 'https://mount-xavier-services.herokuapp.com/addCarousel',
+  getCarouselById: 'https://mount-xavier-services.herokuapp.com/getCarousel/',
+  updateCarousel: 'https://mount-xavier-services.herokuapp.com/updateCarousel/',
+  deleteCarousel:'https://mount-xavier-services.herokuapp.com/deleteCarousel/',
+  contactus:'https://mount-xavier-services.herokuapp.com/contactus',
+  login:'https://mount-xavier-services.herokuapp.com/login'
 };
